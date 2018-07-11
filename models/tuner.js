499ecc8a5ea69d2c07711399ca14898655a26abd
Jordan Mor <jordanmor@gmail.com>
2018-07-11 18:58:52 -0400

    Add model and schema for piano tuner info

---
+++ models/tuner.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const TunerSchema = new Schema({
+    name: {
+        type: String,
+        required: [true, 'Name field is required']
+    },
+    businessName: {
+        type: String
+    },
+    contactInfo: {
+        tel: Number,
+        email: String,
+        businessAddress: {
+            street: String,
+            city: String,
+            state: String,
+            zipCode: String
+        }
+    },
+    available: {
+        type: Boolean,
+        default: false
+    }
+});
+
+const Tuner = mongoose.model('tuner', TunerSchema);
+
+module.exports = Tuner;
