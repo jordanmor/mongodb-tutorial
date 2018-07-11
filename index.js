0db7cc1aa92400933f47153e121f92614242ad55
Jordan Mor <jordanmor@gmail.com>
2018-07-11 18:50:53 -0400

    Connect to mongodb

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
 const app = express();
+
+//connect to mongodb
+mongoose.connect('mongodb://localhost/pianoTunerLocater');
+
+app.use(bodyParser.json());
 
 app.listen(process.env.port || 3000, () => console.log('now listening for requests'));
