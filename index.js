1ac201ee3650defff3a70c0f74de8fdb06c837ec
Jordan Mor <jordanmor@gmail.com>
2018-07-16 17:40:01 -0400

    Add error handler to app

--- index.js
+++ index.js
@@ -12,4 +12,10 @@ app.use(bodyParser.json());
 // initialize routes
 app.use('/api', require('./routes/api'));
 
+//Error handler
+app.use((err, req, res, next) => {
+    console.log(err.message);
+    res.status(422).send({error: err.message});
+});
+
 app.listen(process.env.port || 3000, () => console.log('now listening for requests'));
